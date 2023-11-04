84090d9f0a824965de7832daeb843b65fe933c00
gruszm <mariuszgruszczynski11@gmail.com>
2023-11-04 23:20:52 +0100

    Initial commit

---
+++ src/main/java/pl/morozgrusz/zycieklockow/ZycieklockowApplication.java
@@ -0,0 +1,13 @@
+package pl.morozgrusz.zycieklockow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZycieklockowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZycieklockowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/morozgrusz/zycieklockow/ZycieklockowApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.morozgrusz.zycieklockow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZycieklockowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

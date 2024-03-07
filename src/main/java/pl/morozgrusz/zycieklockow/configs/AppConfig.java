25f99862b5b238a78ea82f64b08b3440952c6c2d
gruszm <mariuszgruszczynski11@gmail.com>
2024-03-07 12:51:35 +0100

    Added Rest endpoint for home page

--- src/main/java/pl/morozgrusz/zycieklockow/configs/AppConfig.java
+++ src/main/java/pl/morozgrusz/zycieklockow/configs/AppConfig.java
@@ -7,7 +7,18 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "app")
 public class AppConfig
 {
+    private String home;
     private String about;
+
+    public String getHome()
+    {
+        return home;
+    }
+
+    public void setHome(String home)
+    {
+        this.home = home;
+    }
 
     public String getAbout()
     {
